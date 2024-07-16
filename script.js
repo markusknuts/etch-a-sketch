dc46a92e8d819a0ef8e359123493036a8155cc61
Markus Knuts <markus.knuts@icloud.com>
2024-07-16 15:48:56 +0200

    forgot to commit but finished the etch a sketch had to rework the javascript

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const divContainer = document.querySelector(".container")   
-const sizeButton = document.querySelector("#gridSize")
+const container = document.getElementById("grid-container");
 
-let originalGridSize = 16
+function random(number) {
+    return Math.floor(Math.random() * number + 1);
+}
 
-createGrid(originalGridSize);
-draw();
+function makeRows(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", function bgChange() {
+            const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+            cell.style.backgroundColor = rndCol;
+        });
+    };
+};
 
+makeRows(16,16);
 
-function createGrid(num) {
-    for (i = 0; i < num; i++) {
-        const row = document.createElement('div')
-        row.classList.add('row')
-        divContainer.appendChild(row)
-        for (k = 0; k < num; k++) {
-            const column = document.createElement('div')
-            column.classList.add('column')
-            row.appendChild(column)
-        } 
+const button = document.getElementById("button");
+
+button.addEventListener("click", function changeSize(size) {
+    size = prompt("Enter a value between 0 to 101");
+    container.innerHTML = "";
+    if (0 < size && size <= 100) {
+        makeRows(size, size);
+    } else {
+        makeRows(16, 16);
+        alert ("Only type values between 1 to 100");
     }
-}
-
-function draw() {
-    const columns = document.getElementsByClassName("column")
-    for (i = 0; i < columns.length; i++) {
-        columns[i].addEventListener("mouseover", changeColor)
-    } 
-}
-
-function changeColor() {
-    this.style.backgroundColor = '#2e2b2b'
-}
-
+});
